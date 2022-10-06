5804fcb3582fe6693fc71f956df4a8abdf7571e9
Harsh Vardhan <75531664+HARSH-nith@users.noreply.github.com>
2022-10-06 22:55:45 +0530

    added reverse_array function

---
+++ 01_Array/reverse_array.cpp
@@ -0,0 +1,11 @@
+void rvereseArray(int arr[], int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}

cd1d01354a58353abcc9d8c31f6c87d0768f9321
shashwat010 <76944311+shashwat010@users.noreply.github.com>
2022-10-07 14:46:19 +0530

    adding code for edit distance

---
+++ DP/edit_distance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]})+1;
+            }
+        }
+        return dp[n][m];
+    }
+};

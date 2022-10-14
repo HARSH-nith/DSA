c546593c7ae832faabedf404b788fa2358eea3ce
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2022-10-14 15:49:39 +0530

    Update dice_throw_problem.cpp

--- DP/dice_throw_problem.cpp
+++ DP/dice_throw_problem.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long lli;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
-typedef vector<lli> vl;
-typedef pair<lli,lli> pll;
-
-#define fi first
-#define se second
-#define pb push_back
-#define mp make_pair
-#define repr(a , b , c) for(int i = a ; i>b ; i-=c)
-#define rep(a , b , c) for(int i = a ; i<b ; i+=c)
-#define print(a) for(auto x:a) cout<<x<<" ";cout<<endl;
-#define sq(a) (a)*(a)
-
 map<vector<int>,int> memo;
 
 int ways(int n ,int m ,int x)
@@ -27,10 +12,10 @@ int ways(int n ,int m ,int x)
 		if(x <= 0) return 0;
 		if(n == 1)
 		{
-			if(x<=m)return 1;
-			else    return 0;
+			if(x<=m) return 1;
+			else     return 0;
 		}
-
+		// recursive call
 		for(int i = 1 ; i<= m ; i ++)
 		{
 			ans += ways(n-1,m,x-i);
@@ -58,16 +43,16 @@ void solve(){
 }
 int main(){
 
-	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	
 
 	#ifndef ONLINE_JUDGE
 		freopen("input.txt", "r" , stdin);
 		freopen("output.txt", "w" , stdout);
 	#endif
 
-	int t;cin>>t; while(t--)
-    solve();
-	
+	int t;cin>>t; 
+	while(t--)
+    {
+    	solve();
+	}
 	return 0;
 }
